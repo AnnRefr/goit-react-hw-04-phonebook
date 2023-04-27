d725092269d50185ed0febe94bf519da1e0920de
AnnRefr <Ann_malaza@ukr.net>
2023-04-27 19:59:58 +0300

    fix

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import css from '../Filter/Filter.module.css';
 
-export function Filter({ name, onChange }) {
+export function Filter({ filter, onChange }) {
   return (
     <label className={css.label}>
       <p>Find contacts by name</p>
-      <input type="text" name={name} onChange={onChange} />
+      <input type="text" value={filter} onChange={onChange} />
     </label>
   );
 }
 
 Filter.propTypes = {
-  name: PropTypes.string,
+  filter: PropTypes.string,
   onChange: PropTypes.func.isRequired,
 };
